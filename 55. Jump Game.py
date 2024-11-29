378a9d05236a0e26b85244d5f8d9b12e198c940d
chickencode18 <dangkhoa18205@gmail.com>
2024-11-29 19:52:32 +0700

    update

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -5,15 +5,18 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        dp = [False] * n
-        dp[0] = True
+        m = 0
 
-        for i in range(1,n):
-            for j in range(i):
-                if dp[j] and j + nums[j] >= i:
-                    dp[i] = True
-                    break
-        return dp[-1]
+
+        for i in range(n):
+            if i > m:
+                return False
+            m = max(m,i+nums[i])
+            if m > n-1:
+                return True
+            
+        return True
+
 
 
 
