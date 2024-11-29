9e535f5bde479c9c58f6252219bf93ca0d5ff794
chickencode18 <dangkhoa18205@gmail.com>
2024-11-29 19:02:17 +0700

    update

---
+++ 53. Maximum Subarray.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        max_sum = dp[0]
+        for i in range(1,n):
+            dp[i] = max(nums[i],nums[i]+dp[i-1])
+            max_sum = max(max_sum,dp[i])
+        return max_sum
