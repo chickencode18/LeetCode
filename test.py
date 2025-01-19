c03017727980c7ed90cbda275a5a97304024c197
chickecode18 <dangkhoa18205@gmail.com>
2025-01-19 16:22:24 +0700

    lc

--- test.py
+++ test.py
@@ -1,22 +1,14 @@
-import math
-
+MOD = 10**9 + 7
 
 def solve():
-    n = int(input())
-    a = list(map(int,input().split()))
+    
 
-    total = sum(a)
-    if total%3==0:
-        print(0,'\n')
-    else:
-        print(3*(total//3 + 1) - total,'\n' )
-         
-            
 
 def main():
-    t = int(input())
-    for _ in range(t):
-        solve()
+    m,n,k = map(int,input().split())
+    for _ in range(k):
+        
+
 
 
 if __name__ == "__main__":
